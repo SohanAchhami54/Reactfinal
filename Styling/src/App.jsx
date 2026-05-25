469c365e2aa6def5133a2513cddf458ab702bb6c
Sohan Ac <sohanachhami655@gmail.com>
2026-05-25 12:39:50 +0545

    feat(styling): learn about css module

---
+++ Styling/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Module from './components/Module'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My name is Sohan Achhami.</h1>
+      <Module/>
+    </div>
+  )
+}
+
+export default App
---
+++ Styling/src/components/Module.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import buttonstyles from './Button.module.css' 
+import uistyles from './Ui.module.css'
+import listyles from './Li.module.css'
+import mainstyles from './Main.module.css'
+const Module = () => { 
+    const users=[
+        {
+            id:1, 
+            name:'Sohan',
+            address:'New Baneshwor'
+        }, 
+        {
+            id:2, 
+            name:'Prabhat',
+            address:'Bhanjang'
+        },{
+            id:3, 
+            name:'Pranaya', 
+            address:'Kathmandu,Bhanjang'
+        }
+    ]
+  return (
+    <div className={mainstyles.main}>
+      <h1>This is module</h1> 
+      <button className={buttonstyles.button}>
+        Click me
+      </button>
+      <ul className={uistyles.ui}>
+        {
+             users.map((user)=>(
+                <li key={user.id} className={listyles.li}>
+                   <h1>Name:{user.name} </h1> 
+                     <h2>Address:{user.address} </h2>
+                </li>
+             ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Module
