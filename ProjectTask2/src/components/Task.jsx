6b85e093506c7011f097988054abbbe057cd0921
Sohan Ac <sohanachhami655@gmail.com>
2026-05-31 17:29:00 +0545

    feat(task): add delete confirmation dialog

--- ProjectTask2/src/components/Task.jsx
+++ ProjectTask2/src/components/Task.jsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react'
 
-const Task = ({task,setTask,input,setInput}) => {  
+const Task = ({task,setTask}) => {  
     const [isEditable,setIsEditable]=useState(false)
     const [editValue,setEditValue]=useState(task.duty)
 
     const handleDelete=(id)=>{
+       const confirm=window.confirm('Are u sure want to delete') 
+       if(!confirm) return 
         setTask(prev=>
             prev.filter(t=>id!==t.id)
         )
     }
   return (
     // Task.jsx
-<div className="flex items-center justify-between  border border-gray-100 rounded-lg px-4 py-3">
+<div className="flex items-center justify-between gap-3 border border-gray-100 rounded-lg px-4 py-3">
     <input type="text"
      value={editValue}
      readOnly={!isEditable}
      onChange={(e)=>setEditValue(e.target.value)}
-     className={`${isEditable?'border rounded-md outline-none':'outline-none'} `}
+     className={` ${isEditable?'border rounded-md outline-none':'outline-none'}  `}
      />
   {/* <span className="text-md text-white">{task.duty}</span> */}
   <div className="flex gap-2">
@@ -30,7 +32,7 @@ const Task = ({task,setTask,input,setInput}) => {
      className="text-md px-3 py-1 rounded-lg border border-indigo-200">
       {isEditable?'Add':'Edit'}
     </button>
-    <button onClick={()=>handleDelete(task.id)}
+    <button onClick={(e)=>handleDelete(task.id)}
      className="text-md text-white px-3 py-1 rounded-lg border border-red-200">
       Delete
     </button>
