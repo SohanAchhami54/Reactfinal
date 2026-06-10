96dde042a84e0767088e48e50a77960806bd82b3
Sohan Ac <sohanachhami655@gmail.com>
2026-06-10 13:31:16 +0545

    refactor(components): create index file for exports

---
+++ ProjectTask3/src/components/index.js
@@ -0,0 +1,6 @@
+import TaskCard from './TaskCard/TaskCard'
+import Stats from './Stats/Stats' 
+import Taskform from './Taskform/Taskform'
+import TaskList from './TaskList/TaskList'
+
+export {TaskCard,Stats,Taskform,TaskList}
