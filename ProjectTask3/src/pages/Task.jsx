0d28120138bb51d50f52c15b51e7c19561a89de7
Sohan Ac <sohanachhami655@gmail.com>
2026-06-10 13:26:06 +0545

    feat(task): integrate taskform and tasklist components

--- ProjectTask3/src/pages/Task.jsx
+++ ProjectTask3/src/pages/Task.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Taskform, TaskList } from '../components'
+import { useTask } from '../context/TaskContext'
 
-const Task = () => {
+
+const Task = () => { 
+  const {task,setTask}=useTask()
   return (
-    <div>
-      Task
+    <div className='flex flex-col sm:flex-col md:flex-row gap-3'>
+      <Taskform  task={task} setTask={setTask} />
+      <TaskList  task={task} />
     </div>
   )
 }
