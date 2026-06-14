a1706d59a12662902c7d89a060eec200b9dd6123
Sohan Ac <sohanachhami655@gmail.com>
2026-06-14 22:41:17 +0545

    feat(app): add router and queryclient to whole app

--- 20ReactQuery/src/App.jsx
+++ 20ReactQuery/src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import {createBrowserRouter,createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import Weather from './components/Weather'
+import Todos from './components/Todos'
+import Main from './components/layout/Main'
 
 const App = () => {
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Main/>}>
+           <Route index element={<Weather/>} />
+           <Route path='/todos' element={<Todos/>} />
+        </Route>
+    )
+  )
   const queryClient=new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
-        <Weather queryClient={queryClient} />
+        <RouterProvider router={router}/>
     </QueryClientProvider>
   )
 }
