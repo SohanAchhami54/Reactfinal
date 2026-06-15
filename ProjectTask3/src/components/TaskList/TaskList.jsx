b55fd5c04eca44b28f82f7f3aaec2579e4788aa8
Sohan Ac <sohanachhami655@gmail.com>
2026-06-15 19:25:20 +0545

    feat(task-list): add completed and pending task filters

--- ProjectTask3/src/components/TaskList/TaskList.jsx
+++ ProjectTask3/src/components/TaskList/TaskList.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTask } from '../../context/TaskContext'
+import TaskCard from '../TaskCard/TaskCard'
 
-const TaskList = ({task}) => {
-  return (
-    <div className='flex-auto'>
-      <ul className='p-5 flex flex-wrap gap-4 justify-center items-start '>
+const TaskList = () => {
+    const {task}=useTask()
+
+    const [search,setSearch]=useState('')
+    const [filter,setFilter]=useState('all')
+
+    const filtered=task.filter(t=>{
+      const matchsearch=t.title.toLowerCase().includes(search.toLowerCase())
+      const matchfiler=filter==='all'?true:filter==='completed'?t.completed:!t.completed
+      return matchsearch&&matchfiler
+    })
+
+   
+ return (
+    <div className='p-4 space-y-4'>
+        {/* for search and toggle  */}  
+
+        <div className='flex gap-2 '>
+          <input type="text" 
+          value={search} 
+          onChange={(e)=>setSearch(e.target.value)}
+          placeholder='Search the task'
+
+          className='border outline-none focus:ring-2 w-full max-w-md py-2 rounded-md'
+           />
+
+           <select value={filter}
+            onChange={(e)=>setFilter(e.target.value)}
+            className='bg-black text-white'
+            >
+            <option value="all">All</option>
+            <option value="pending">Pending</option>
+            <option value="completed">Completed</option>
+           </select>
+            </div>
+
+      <ul className='flex flex-wrap gap-4 '>
+
          {
-          task?.map((t)=>(
-            <li key={t.id}>
-                <h1>Task:{t.title} </h1>
-                <h1>Description:{t.description} </h1>
-                <p>Priority:{t.priority}</p> 
-                <p>CreatedAt:{t.createdAt} </p>
-            </li>
+          filtered?.map((t)=>(
+             <TaskCard key={t.id} t={t}/>
           ))
          }
       </ul>
