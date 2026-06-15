396bf7fad2503acf2c030b6b7c49f6aa2f6c038e
Sohan Ac <sohanachhami655@gmail.com>
2026-06-15 19:27:10 +0545

    feat(theme): add ThemeProvider to application root

--- ProjectTask3/src/main.jsx
+++ ProjectTask3/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { TaskProvider } from './context/TaskContext.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <TaskProvider>
+      <ThemeProvider>
        <App />
+       </ThemeProvider>
     </TaskProvider>
   </StrictMode>,
 )
