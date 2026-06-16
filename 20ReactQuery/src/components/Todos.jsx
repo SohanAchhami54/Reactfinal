49ff01820e71de587d0355816c4a562823036845
Sohan Ac <sohanachhami655@gmail.com>
2026-06-16 22:29:04 +0545

    feat(todo): update cache after adding new todo

--- 20ReactQuery/src/components/Todos.jsx
+++ 20ReactQuery/src/components/Todos.jsx
@@ -18,11 +18,15 @@ const Todos = () => {
             const response= await Axios.post('/', newTodo)
             return response.data
          },
-         onSuccess:()=>{
-            queryClient.invalidateQueries({queryKey:['todos']})
+         onSuccess:(returnTodo)=>{
+          queryClient.setQueryData(['todos'], (oldTodos = []) => {
+          return [...oldTodos, returnTodo];
+    });
          }
     })
     const {data,isLoading,isError}=query 
+    const {isPending,mutate}=mutation
+
 
     if(isLoading) return <p>Loading please wait </p>
     if(isError) return <p className='text-4xl text-red-500'>Error occur please try again later. </p>
@@ -35,7 +39,6 @@ const Todos = () => {
             data?.map((d)=>(
                 <li key={d.id} className='p-3 bg-gray-900 rounded-md'>
                     <div className='flex flex-col gap-3 bg-gray-800 rounded-md'>
-                          <p>Completed:{d.completed}</p>
                          <p>Title:{d.title} </p>
                          <p>userId:{d.userId} </p>
                     </div>
@@ -46,12 +49,12 @@ const Todos = () => {
        </ul>
        <button onClick={()=>{
            mutation.mutate({
-            id:new Date(),
+            userId:new Date(),
             title:'Buy new Laptop',
            })
        }}
          className='ml-5 px-2 py-1 rounded-md bg-gray-900'>
-         Add Todo
+            {isPending?'Adding Todo':'Add Todo'}
        </button>
     </div>
   )
