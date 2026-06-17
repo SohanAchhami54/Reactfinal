75551d24766b1bf74c7a43e3833d07a2d140547c
Sohan Ac <sohanachhami655@gmail.com>
2026-06-17 16:20:54 +0545

    chore(app): remove  heading text

--- ProjectTask2.1/src/App.jsx
+++ ProjectTask2.1/src/App.jsx
@@ -4,7 +4,7 @@ import Crud from './pages/Crud'
 const App = () => {
   return (
     <div className='min-h-screen bg-[#023047] text-white'>
-       <h1>My name is Sohan Achhami.</h1>
+
        <Crud/>
     </div>
   )
