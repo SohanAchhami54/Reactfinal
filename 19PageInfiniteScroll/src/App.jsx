55bb5cccfc92ee7a495b5c652d473d1974a48033
Sohan Ac <sohanachhami655@gmail.com>
2026-06-17 15:30:14 +0545

    feat(router): add pagination router in app

--- 19PageInfiniteScroll/src/App.jsx
+++ 19PageInfiniteScroll/src/App.jsx
@@ -3,12 +3,14 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import InfiniteScroll from './pages/InfiniteScroll'
 import About from './pages/About'
 import Main from './components/layout/Main'
+import Pagination from './pages/Pagination'
 
 const App = () => {
   const router=createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<Main/>}>
           <Route index element={<InfiniteScroll/>} />
+          <Route path='/pagination' element={<Pagination/>} />
           <Route path='/about' element={<About/>} />
       </Route>
     )
