7ed84d31636182b02a9b1ff065931f48a1b5eb11
Sohan Ac <sohanachhami655@gmail.com>
2026-06-17 15:51:09 +0545

    style(notfound): enhance 404 page ui

--- ProjectTask3/src/pages/Task.jsx
+++ ProjectTask3/src/pages/Task.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Taskform, TaskList } from '../components'
+import { Stats, Taskform, TaskList } from '../components'
 import { useTask } from '../context/TaskContext'
 
 
 const Task = () => { 
-  const {task,setTask}=useTask()
   return (
     <div className='flex flex-col sm:flex-col md:flex-row gap-3'>
-      <Taskform  task={task} setTask={setTask} />
-      <TaskList  task={task} />
+      <Taskform/>
+       <div>
+           <TaskList/>
+           <Stats/>
+       </div>
+    
     </div>
   )
 }
