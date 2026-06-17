016bf745c7ac6a5f4d1e910f70ce6d9b0f1ad7ff
Sohan Ac <sohanachhami655@gmail.com>
2026-06-17 15:38:49 +0545

    chore(redux): setup redux folder structure

---
+++ 21Redux/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-gray-600'>
+      <h1>This is redux toolkit. </h1>
+    </div>
+  )
+}
+
+export default App
