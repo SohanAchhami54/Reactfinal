a7a874530e8f0c4d4d42706b7dd7e14015362d26
Sohan Ac <sohanachhami655@gmail.com>
2026-06-17 15:46:56 +0545

    feat(about): create about me profile section

--- ProjectTask3/src/pages/AboutMe.jsx
+++ ProjectTask3/src/pages/AboutMe.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 export default function AboutMe() {
   return (
-    <div>
-      Sohan Achhami 
-      frontend developers
+    <div >
+      <div className='flex flex-col gap-5 justify-center items-center'>
+          <h1 className='text-3xl font-medium'>Sohan Achhami</h1>
+          <p className='text-xl'>Internship Role : Software Developer</p>
+          <p className='text-sm'>Technologies Learn :HTML, CSS, javascript, React.js, Tailwindcss, React Router, React Query </p>
       
+      </div>
+    
     </div>
   )
 }
