db63e2454cc6d1a8196484962eea6082d59efbb2
Sohan Ac <sohanachhami655@gmail.com>
2026-06-17 19:11:36 +0545

    feat(redux): add counter and cart commponents to app

--- 21Redux/src/App.jsx
+++ 21Redux/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Counter from './components/Counter'
+import Cart from './components/Cart'
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-gray-600'>
-      <h1>This is redux toolkit. </h1>
+    <div className='min-h-screen bg-gray-600 text-white p-3'>
+      <h1 className='text-3xl'>This is redux toolkit. </h1>
+      <div className='flex  flex-col gap-4'>
+        <Counter/> 
+       <Cart/>
+      </div>
+       
     </div>
   )
 }
