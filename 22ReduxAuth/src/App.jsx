685beb1d5f9a0eac276d0b2c4bb08f7232d25a1d
Sohan Ac <sohanachhami655@gmail.com>
2026-06-18 17:02:55 +0545

    feat(app): set up application routing with auth guards

--- 22ReduxAuth/src/App.jsx
+++ 22ReduxAuth/src/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
-
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import Main from './components/layout/Main'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import PublicRoute from './components/PublicRoute'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './pages/Home'
+import ErrorPage from './components/ErrorPage'
 const App = () => {
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Main/>} >
+        <Route index element={<Home/>} />
+
+        <Route path='/login' element={
+          <PublicRoute>
+              <Login/>
+          </PublicRoute>
+         }/>
+        <Route path='/profile' element={
+          <ProtectedRoute>
+                   <Profile/>
+          </ProtectedRoute>
+
+          }/>
+          <Route path='*' element={<ErrorPage/>} />
+      </Route>
+    )
+  )
   return (
-    <div>
-       <h1>This is App.</h1>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
