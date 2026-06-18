f064de121e09a75898b06f78f9daac7b5c350fa6
Sohan Ac <sohanachhami655@gmail.com>
2026-06-18 11:38:11 +0545

    feat(app): add route base navigation for counter, cart and items pages

--- 21Redux/src/App.jsx
+++ 21Redux/src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import Counter from './components/Counter'
 import Cart from './components/Cart'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Items from './components/Items'
+import Main from './components/layout/Main'
 
 const App = () => {
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Main/>} >
+        <Route  index element={<Counter/>} />
+        <Route  path='/cart' element={<Cart/>} />
+        <Route  path='/items' element={<Items/>} />
+      </Route>
+    )
+  )
   return (
-    <div className='min-h-screen bg-gray-600 text-white p-3'>
-      <h1 className='text-3xl'>This is redux toolkit. </h1>
-      <div className='flex  flex-col gap-4'>
-        <Counter/> 
-       <Cart/>
-      </div>
-       
-    </div>
+     <RouterProvider router={router} />
   )
 }
 
