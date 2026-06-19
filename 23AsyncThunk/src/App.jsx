331de0a6532d7fd1e39f70955c4041f4fb62882d
Sohan Ac <sohanachhami655@gmail.com>
2026-06-19 14:54:17 +0545

    chore(redux):setup files for async thunks

---
+++ 23AsyncThunk/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-800 min-h-screen text-white p-3'>
+      <h1>My name is Sohan Achhami.</h1>
+    </div>
+  )
+}
+
+export default App
