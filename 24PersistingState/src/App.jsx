b018279849f0c520858e7042d2bcd1a31526a684
Sohan Ac <sohanachhami655@gmail.com>
2026-06-22 23:39:24 +0545

    feat(route): configure application routers with react-router

--- 24PersistingState/src/App.jsx
+++ 24PersistingState/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Main from './components/layout/Main'
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
 
 const App = () => {
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Main/>}>
+           <Route index element={<Home/>}/>
+           <Route path='/about' element={<About/>}/>
+           <Route path='/login' element={<Login/>}/>
+      </Route>
+    )
+  )
   return (
-    <div>
-      <h1>This is about Persisting State.</h1>
-    </div>
+     <RouterProvider router={router}/>
   )
 }
 
