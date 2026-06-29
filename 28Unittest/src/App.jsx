039f77e04c51c945c93b74ba297b5b55f6bea233
Sohan Ac <sohanachhami655@gmail.com>
2026-06-29 17:11:50 +0545

    feat(app): render task page in app

--- 28Unittest/src/App.jsx
+++ 28Unittest/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Task from './pages/Task'
 const App = () => {
   return (
     <div className='bg-gray-700 min-h-screen text-white'>
       <h1>This is unit testing.</h1>
+      <Task/>
     </div>
   )
 }
