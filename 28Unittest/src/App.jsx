0dd443372e174647bea81806af04f0844ddeb3f4
Sohan Ac <sohanachhami655@gmail.com>
2026-06-29 13:10:31 +0545

    chore(test): initialize unit testing setup

---
+++ 28Unittest/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-700 min-h-screen text-white'>
+      <h1>This is unit testing.</h1>
+    </div>
+  )
+}
+
+export default App
