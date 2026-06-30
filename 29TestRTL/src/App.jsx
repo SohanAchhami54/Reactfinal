5451f6f6126c888c2520005e2da25141839ca585
Sohan Ac <sohanachhami655@gmail.com>
2026-06-30 18:25:16 +0545

    feat(router): set up routing with react router

--- 29TestRTL/src/App.jsx
+++ 29TestRTL/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import {createBrowserRouter,createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import Main from './components/layout/Main'
+import Form from './pages/Form'
+import Counter from './pages/Counterpage'
+import Usercard from './pages/Usercard'
+import Home from './pages/Home'
+
 const App = () => {
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Main/>}>
+            <Route index element={<Home/>}/> 
+             <Route path='/usercard' element={<Usercard/>}/>
+             <Route path='/counter' element={<Counter/>} /> 
+             <Route path='/form' element={<Form/>} />
+        </Route>
+    )
+  )
   return (
-    <div>
-       <h1>This is testing with rtl. </h1>
-    </div>
+   <RouterProvider router={router}/>
   )
 }
 
