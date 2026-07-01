fb583facc18c9fe8150fa8ca83810349c7256a79
Sohan Ac <sohanachhami655@gmail.com>
2026-07-01 22:43:50 +0545

    feat(page): create home, about and blog pages

---
+++ 30SnapMockTest/src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Blogpost from '../components/Blogpost'
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+        <Blogpost/>
+    </div>
+  )
+}
+
+export default Blog
