51ef5a60ddc5b3ec6c58730781c6fc5644721996
Sohan Ac <sohanachhami655@gmail.com>
2026-07-01 22:45:30 +0545

    feat(app): initialize routing and react query

--- 30SnapMockTest/src/App.jsx
+++ 30SnapMockTest/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import Main from './components/layout/Main'
+import Home from './pages/Home'
+import About from './pages/About'
+import Blog from './pages/Blog'
 
+const queryClient=new QueryClient()
 const App = () => {
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Main/>}>
+          <Route index element={<Home/>}/> 
+          <Route path='/about' element={<About/>}/> 
+          <Route path='/blogpost' element={<Blog/>}/> 
+      </Route>
+    )
+  ) 
+ 
   return (
-    <div>
-      <h1>This is snapshot and mock api call.</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}/>
+   </QueryClientProvider>
   )
 }
 
