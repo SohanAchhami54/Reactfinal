54a4f1ffca8d224d82eaa7bad8383cdb4bc8fff8
Sohan Ac <sohanachhami655@gmail.com>
2026-07-06 17:00:25 +0545

    feat(routing): implement route-based lazy loading with React.lazy

--- 32lazyloading/src/App.jsx
+++ 32lazyloading/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { lazy } from 'react'
+import {createBrowserRouter,createRoutesFromElements,RouterProvider,Route}  from 'react-router-dom'
+import Main from './components/layout/Main' 
 
-const App = () => {
+//split code and when to fetch.
+const Home=lazy(()=>import('./components/Home')) 
+const About=lazy(()=>import('./components/About')) 
+const Contact=lazy(()=>import('./components/Contact')) 
+const Dashboard=lazy(()=>import('./components/Dashboard')) 
+
+const App = () => { 
+  const router=createBrowserRouter(
+      createRoutesFromElements(
+        <Route path='/' element={<Main/>}>
+          <Route index element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Route>
+   )
+)
   return (
-    <div>
-      <h1>This is code splitting and lazy loading.</h1>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
-
 export default App
