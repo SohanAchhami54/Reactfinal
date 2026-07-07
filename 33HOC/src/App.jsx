698897ae9dda812eb27f319890f7c28673d2cbf9
Sohan Ac <sohanachhami655@gmail.com>
2026-07-07 17:42:56 +0545

    feat(router): configure application routes with protected dashboard

--- 33HOC/src/App.jsx
+++ 33HOC/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Main from './components/layout/Main'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Login from './components/Login'
+import ProtectedDashboard from './components/Dashboard'
 
-const App = () => {
+const App = () => { 
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Main/>}>
+          <Route index  element={<Home/>}/>
+          <Route path='/about'  element={<About/>}/>
+          <Route path='/contact'  element={<Contact/>}/>
+          <Route path='/login'  element={<Login/>}/>
+          <Route path='/dashboard' element={<ProtectedDashboard/>}/>
+      </Route>
+    )
+  )
   return (
-    <div>
-      <h1>This is higher order components.</h1>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
-
 export default App
