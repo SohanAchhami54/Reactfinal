7ea75151c6ba3fdc54f2020c585d2d7e33e910a4
Sohan Ac <sohanachhami655@gmail.com>
2026-07-07 17:39:34 +0545

    feat(auth): protected dashboard route with hoc

---
+++ 33HOC/src/components/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import withAuth from '../hoc/withAuth'
+
+
+const Dashboard = () => { 
+    const auth=useSelector(state=>state.auth)
+   return (
+    <div>
+      <h1>This is dashboard</h1> 
+       <span>{auth.user.email} </span>
+    </div>
+  )
+}
+
+const ProtectedDashboard=withAuth(Dashboard) 
+export default ProtectedDashboard
