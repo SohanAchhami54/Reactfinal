75a78420b7f904fe4ae8e9514ee38a9df1b8d535
Sohan Ac <sohanachhami655@gmail.com>
2026-07-09 11:42:11 +0545

    feat(dashboard): display modal using createPortal

--- 33HOC/src/components/Dashboard.jsx
+++ 33HOC/src/components/Dashboard.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import withAuth from '../hoc/withAuth'
+import Modal from './Modal'
 
 
 const Dashboard = () => { 
     const auth=useSelector(state=>state.auth)   
-    // throw new Error('Testing Environment')
+    // throw new Error('Testing Environment') 
+    const [isOpen,setIsOpen]=useState(false)
    return (
     <div>
       <h1>This is dashboard</h1> 
-       <span>{auth.user.email} </span>
+       <span>{auth.user.email} </span> 
+        
+        <br/>
+
+        <button onClick={()=>setIsOpen(true)}
+        className='py-2 px-1 bg-blue-700 '>
+             Open Modal
+        </button>
+
+       <Modal
+       isOpen={isOpen} 
+       onClose={()=>setIsOpen(false)}
+       > 
+        <h2 className='text-2xl font-bold'>Welcome</h2>
+        <p>This modal use createPortal.</p>
+       </Modal>
     </div>
   )
 }
