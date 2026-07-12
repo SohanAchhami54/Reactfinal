1a194cc03d8ae97bb5000cd7f08c9313b41a1dc8
Sohan Ac <sohanachhami655@gmail.com>
2026-07-12 16:07:51 +0545

    feat(blog): add blog listing page

--- 34EnvConfig/src/App.jsx
+++ 34EnvConfig/src/App.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Axios } from './api/axios'
 
-const App = () => {
+const App = () => {  
+
+  const [data,setData]=useState([])
+
+  const getData=async()=>{ 
+    try {
+        const reponse=await Axios.get()
+       setData(reponse.data)
+    } catch (error) {
+      console.log('error occur:',error)
+    }
+    
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+ 
+  console.log('the data is:',data)
+
   return (
-    <div>
-      <h1>This Env and config.</h1>
+    <div className='p-4'>
+      <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 '>
+        {
+          data?.slice(0,20).map((d)=>(
+            <li key={d.id} className='flex flex-col gap-3 bg-gray-500 text-white p-3 rounded-md group '>
+               <h1>Category: {d.category}</h1>
+                <div className='rounded-md overflow-hidden'>
+                  <img src={d.featured_image} alt={d.subtitle}
+                  className='rounded-md group-hover:scale-105 transition-all duration-200 ease-in delay-100' />
+                </div>
+                <span className='text-neutral-800 text-sm'>{d.subtitle}</span>
+                <span className='line-clamp-3 text-neutral-800 text-sm'>{d.summary } </span>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
