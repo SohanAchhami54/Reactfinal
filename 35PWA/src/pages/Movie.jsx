e739578be6237457ec6acf06eb156053ecaf9915
Sohan Ac <sohanachhami655@gmail.com>
2026-07-14 15:12:24 +0545

    feat(pages): add home,about and movie pages

---
+++ 35PWA/src/pages/Movie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MovieBlog from '../components/MovieBlog'
+
+const Movie = () => {
+  return (
+    <div>
+      <MovieBlog/>
+    </div>
+  )
+}
+
+export default Movie
