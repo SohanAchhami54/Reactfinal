233a485abf17f86602b1e8ba48cda9ec8e720b90
Sohan Ac <sohanachhami655@gmail.com>
2026-07-14 15:13:20 +0545

    feat(app): setup queryclientprovider and routerprovider

--- 35PWA/src/App.jsx
+++ 35PWA/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
+import Main from './components/layout/Main'
+import Home from './pages/Home'
+import About from './pages/About'
+import Movie from './pages/Movie'
 
-const App = () => {
+const queryClient=new QueryClient()
+const App = () => { 
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Main/>}>
+        <Route index element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/movie' element={<Movie/>} />
+      </Route>
+    )
+  )
   return (
-    <div>
-      <h1>My name is Sohan Achhami.</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}/>
+    </QueryClientProvider>
   )
 }
 
