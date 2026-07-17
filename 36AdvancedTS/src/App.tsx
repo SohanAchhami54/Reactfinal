5b3ae51cfce9b023330cb17bb79c2b9d79156c23
Sohan Ac <sohanachhami655@gmail.com>
2026-07-17 17:34:28 +0545

    feat(app): display Todolist and Userlist

--- 36AdvancedTS/src/App.tsx
+++ 36AdvancedTS/src/App.tsx
@@ -1,9 +1,11 @@
+import TodoList from "./components/TodoList"
+import UserList from "./components/UserList"
 
 const App = () => {
   return (
-    <div className="text-xl p-3 min-h-screen bg-gray-700 text-white">
-       <h1 className="">This is Advanced Typescript.</h1>
-       <span className="text-xl ">Hello</span>      
+    <div className="text-xl p-3 min-h-screen bg-gray-700 text-white"> 
+       <TodoList/>   
+       <UserList/>
     </div>
   )
 }
