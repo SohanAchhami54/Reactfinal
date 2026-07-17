07aac58ee2ef18d0bd5f8df4486d9a8e5ccddcbc
Sohan Ac <sohanachhami655@gmail.com>
2026-07-17 22:39:39 +0545

    feat(router): add nested routing with main layout

--- 36AdvancedTS/src/App.tsx
+++ 36AdvancedTS/src/App.tsx
@@ -1,12 +1,25 @@
-import TodoList from "./components/TodoList"
-import UserList from "./components/UserList"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Main from "./components/layout/Main"
+import Home from "./pages/Home"
+import User from "./pages/User"
+import Todo from "./pages/Todo"
+import Login from "./pages/Login"
+import ErrorHandle from "./components/ErrorHandle"
 
-const App = () => {
+const App = () => { 
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Main/>} >
+       <Route index element={<Home/>} />
+       <Route path='/todo' element={<User/>}/>
+       <Route path='/user' element={<Todo/>}/>
+       <Route path='/login' element={<Login/>}/>
+       <Route path='*' element={<ErrorHandle/>} />
+      </Route>
+    )
+  )
   return (
-    <div className="text-xl p-3 min-h-screen bg-gray-700 text-white"> 
-       <TodoList/>   
-       <UserList/>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
