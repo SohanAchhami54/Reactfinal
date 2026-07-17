582a9c5db7868d06496c5c082d864dbeb713fe1d
Sohan Ac <sohanachhami655@gmail.com>
2026-07-17 12:21:30 +0545

    chore(ts): initialize react typescirpt project

---
+++ 36AdvancedTS/src/App.tsx
@@ -0,0 +1,11 @@
+
+const App = () => {
+  return (
+    <div className="text-xl p-3 min-h-screen bg-gray-700 text-white">
+       <h1 className="">This is Advanced Typescript.</h1>
+       <span className="text-xl ">Hello</span>      
+    </div>
+  )
+}
+
+export default App
