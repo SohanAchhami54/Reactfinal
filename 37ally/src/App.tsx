b7cc11f9741315839b4c3ebdf997de055b837e16
Sohan Ac <sohanachhami655@gmail.com>
2026-07-21 17:13:47 +0545

    feat(app): integrate login and accessible modal components

--- 37ally/src/App.tsx
+++ 37ally/src/App.tsx
@@ -1,7 +1,12 @@
+import AccessibleModal from "./components/AccessibleModal"
+import Login from "./components/Login"
+
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-800 text-white p-3">
       <h1>This is react.js</h1>
+       <Login/>
+       <AccessibleModal/>
     </div>
   )
 }
