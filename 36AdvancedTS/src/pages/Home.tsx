9bcbddbbb5d7c2d2ce03e4b2d063fc3011d83ff2
Sohan Ac <sohanachhami655@gmail.com>
2026-07-21 13:45:49 +0545

    feat(counter): add typed state and action with useReducer

--- 36AdvancedTS/src/pages/Home.tsx
+++ 36AdvancedTS/src/pages/Home.tsx
@@ -1,8 +1,37 @@
+import { useReducer } from "react"
 
-const Home = () => {
+  type State={
+    count:number 
+  }
+   
+  type Action= {type:'increment'} | {type:'decrement'} // union type.
+  
+  function reducer(state:State,action:Action):State{
+     switch(action.type){
+        case 'increment':
+          return {count:state.count+1}
+
+        case 'decrement': 
+         return {count:state.count-1} 
+         
+         default: 
+          return state
+     }  
+  }
+
+
+const Home = () => {  
+  const [state,dispatch]=useReducer(reducer,{count:0})
   return (
     <div>
-      Home
+      <h1>Home page</h1>
+         <h1>Count: {state.count}</h1>
+           <button onClick={()=>dispatch({type:'increment'})}>
+             Increment
+           </button>
+           <button onClick={()=>dispatch({type:'decrement'})}>
+            Decrement
+           </button>
     </div>
   )
 }
