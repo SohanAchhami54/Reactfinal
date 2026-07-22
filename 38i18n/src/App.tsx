22ae2b831f7b1fd4c4c76c9a98c366d7863466d4
Sohan Ac <sohanachhami655@gmail.com>
2026-07-22 11:48:27 +0545

    chore(i18n): initialize i18n setup

---
+++ 38i18n/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-700 text-white p-3">
+      <h1>This is internalization Project.</h1>
+    </div>
+  )
+}
+
+export default App
