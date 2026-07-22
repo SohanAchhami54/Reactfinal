496dfd01d8d6307171f322efded0e83d43fca09b
Sohan Ac <sohanachhami655@gmail.com>
2026-07-22 15:19:24 +0545

    feat(router): add main layout and page routing

--- 38i18n/src/App.tsx
+++ 38i18n/src/App.tsx
@@ -1,8 +1,21 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+import Main from "./components/layout/Main"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+
 const App = () => {
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Main/>}>
+          <Route index element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+      </Route>
+    )
+  )
   return (
-    <div className="min-h-screen bg-gray-700 text-white p-3">
-      <h1>This is internalization Project.</h1>
-    </div>
+   <RouterProvider router={router}/>
   )
 }
 
