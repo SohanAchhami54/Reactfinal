da9342381e506c9965e3e69309b8d4334ea3d005
Sohan Ac <sohanachhami655@gmail.com>
2026-07-24 17:43:57 +0545

    feat(main): integrate redux provider and persistgate with app

--- 39Portfolio/src/main.tsx
+++ 39Portfolio/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.tsx'
-
+import { persistor, store } from './app/store.ts'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import "./i18n";
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+             <App />
+      </PersistGate>
+    </Provider> 
   </StrictMode>,
 )
