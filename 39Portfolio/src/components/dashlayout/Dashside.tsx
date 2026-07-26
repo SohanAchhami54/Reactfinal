b15cae67bfcad3cd2038a1caf4968ff1bf280ee9
Sohan Ac <sohanachhami655@gmail.com>
2026-07-26 10:37:31 +0545

    style(sidebar): improve sidebar spacing with responsive padding

--- 39Portfolio/src/components/dashlayout/Dashside.tsx
+++ 39Portfolio/src/components/dashlayout/Dashside.tsx
@@ -7,7 +7,7 @@ const Dashside = () => {
       return isActive?'text-red-500 flex items-center justify-center gap-2 ':'flex items-center justify-center gap-2'
   }
   return (
-    <div className="flex flex-col items-start gap-2 min-h-screen bg-gray-800 p-4 rounded-md">
+    <div className="flex flex-col items-start gap-2 min-h-screen bg-gray-800 p-2 md:p-4 rounded-md">
        
          <NavLink className={isActive} to='blog'>
           <span className="hidden md:flex">Blog</span>
