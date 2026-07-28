82683b32e3c9b6492498072105e87dce1dcd6476
Sohan Ac <sohanachhami655@gmail.com>
2026-07-28 13:11:25 +0545

    perf(code-splitting): lazy load route components

--- 39Portfolio/src/App.tsx
+++ 39Portfolio/src/App.tsx
@@ -1,17 +1,21 @@
 import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from "react-router-dom"
-import Main from "./components/layout/Main"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Dashboard from "./pages/Dashboard"
-import Contact from "./pages/Contact"
-import Login from "./pages/Login"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import Error from "./components/Error"
+import Main from "./components/layout/Main"
 import ProtectedRoute from "./components/ProtectedRoute"
 import PublicRoute from "./components/PublicRoute"
 import Weather from "./pages/Weather"
-import BlogDetail from "./components/BlogDetail"
-import Blog from "./pages/Blog"
+import { lazy } from "react"
+
+
+const Home=lazy(()=>import('./pages/Home.tsx'))  
+const About=lazy(()=>import('./pages/About.tsx'))
+const Dashboard=lazy(()=>import('./pages/Dashboard.tsx')) 
+const Contact=lazy(()=>import('./pages/Contact.tsx')) 
+const Login=lazy(()=>import('./pages/Login.tsx')) 
+const Error=lazy(()=>import('./components/Error.tsx')) 
+const Blog=lazy(()=>import('./pages/Blog.tsx')) 
+const BlogDetail=lazy(()=>import('./components/BlogDetail.tsx'))
+
 
 const queryclient=new QueryClient()
 const App = () => { 
@@ -38,9 +42,8 @@ const App = () => {
   )
   return (
     <QueryClientProvider client={queryclient}>
-         <RouterProvider router={router}/>
+          <RouterProvider router={router}/>
     </QueryClientProvider>
-   
   )
 }
 export default App
