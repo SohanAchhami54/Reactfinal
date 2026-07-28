5535c649fcb2583d5f8f3167e9d5c40ec7ecea32
Sohan Ac <sohanachhami655@gmail.com>
2026-07-28 13:09:10 +0545

    feat(layout): wrap outlet with lazyload component

--- 39Portfolio/src/components/layout/Main.tsx
+++ 39Portfolio/src/components/layout/Main.tsx
@@ -1,15 +1,18 @@
 import { Outlet, ScrollRestoration } from "react-router-dom"
 import Header from "./Header"
 import Footer from "./Footer"
+import LazyLoad from "../LazyLoad"
 
 const Main = () => {
   return (
     <div className="text-white bg-gray-700">
        <Header/>
          <main className="min-h-screen pt-15 p-3">
-            <Outlet/>
+             <LazyLoad>
+                <Outlet/>
+           </LazyLoad>
          </main>
-         <Footer/>
+        <Footer/>
           <ScrollRestoration/>
     </div>
   )
