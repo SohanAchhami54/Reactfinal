22653a24de217425ecda4ccd76ce067583ae3b40
Sohan Ac <sohanachhami655@gmail.com>
2026-07-28 13:07:59 +0545

    feat(loader): add centered page loading component

---
+++ 39Portfolio/src/components/PageLoader.tsx
@@ -0,0 +1,11 @@
+import CircularEnableTrack from "../ui/Circular"
+
+const PageLoader = () => {
+  return (
+    <div className="flex justify-center items-center mt-16"> 
+       <CircularEnableTrack/> 
+    </div>
+  )
+}
+
+export default PageLoader
