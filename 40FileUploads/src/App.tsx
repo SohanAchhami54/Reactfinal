21226ea083eaf2cc95f35b13b08b69b0281dae39
Sohan Ac <sohanachhami655@gmail.com>
2026-07-30 14:35:25 +0545

    feat(app): add drag-and-drop placement and sortable list

--- 40FileUploads/src/App.tsx
+++ 40FileUploads/src/App.tsx
@@ -1,9 +1,14 @@
 import FileUpload from "./components/FileUpload"
 
+import DnDPlacement from "./components/DnDPlacement"
+import { SortableList } from "./components/SortableList"
+
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-800 text-white p-2">
+    <div className="min-h-screen flex flex-col gap-4 bg-gray-800 text-white p-2">
       <FileUpload/>
+      <DnDPlacement/>
+      <SortableList/>
     </div>
   )
 }
